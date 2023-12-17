5d2e2c0fb7f38e1708fa37579e0f31a75eadd5eb
Oleg <Asaahi.work@gmail.com>
2023-12-17 15:15:14 +0300

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import '../styles/globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Danone Markest App',
+  title: 'Danone Markessst App',
   description: 'Best Marketplace',
 }
 
