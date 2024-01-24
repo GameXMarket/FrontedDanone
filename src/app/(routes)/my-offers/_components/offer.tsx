d387450836ae82c553427b04d7e3b3b1f4132115
Oleg <Asaahi.work@gmail.com>
2024-01-25 01:01:50 +0300

    user-orders by category

--- src/app/(routes)/my-offers/_components/offer.tsx
+++ src/app/(routes)/my-offers/_components/offer.tsx
@@ -1,9 +1,11 @@
 import { FC } from "react";
 import styles from '../styles/page.module.css'
 import Image from "next/image";
+import Link from "next/link";
 
 const Offer:FC = () => {
     return (
+        <Link href='/my-offers/category_id'>
         <div className={styles.offer}>
             <div className="flex items-center justify-center ml-3">
                 <Image src='/game-assets/game.svg' alt='game' width={102} height={98}/>
@@ -13,6 +15,7 @@ const Offer:FC = () => {
                 <p>17 лотов на продаже</p>
             </div>
         </div>
+        </Link>
     )
 }
 
