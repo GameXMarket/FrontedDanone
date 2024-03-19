3219d90630e1853eb915bdb15b795ce084863db7
Oleg <Asaahi.work@gmail.com>
2024-03-19 16:39:13 +0300

    uploading avatar for user

--- src/requests/attachment/attachment-service.ts
+++ src/requests/attachment/attachment-service.ts
@@ -1,13 +1,14 @@
 import { createSafeFetch } from "@/lib/create-safe-fetch"
 import instance from ".."
-import {UploadFileDto, uploadFilesSchema} from "./schemas"
-import { headers } from "next/headers"
+import {UploadFileDto, uploadFilesSchema, UploadUserFileDto} from "./schemas"
+import { toFormData } from "axios"
 
 export const AttachmentApiService = {
 
     async uploadOfferImage(data: UploadFileDto) {
         const formData = new FormData()
         formData.append('files', data.files[0])
+        console.log(formData)
         return instance.post(`attacment/uploadfiles/offer?offer_id=${data.offer_id}`, formData, {
             headers: {
                 "Content-Type": "multipart/form-data"
@@ -25,6 +26,17 @@ export const AttachmentApiService = {
         return instance.get(`attacment/getfile?file_id=${file_id}`, {headers: {Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDk3NTQwMTMsInN1YiI6Im1yYW1vcjkxOTMyNzY1QGdtYWlsLmNvbSIsInNlc3Npb24iOiJjYTA3NmU1Ny1hMTQ2LTQ3OTctYWNmMC1hYjA3NGNhNDA2OTMiLCJ0b2tlbl90eXBlIjoiYWNjZXNzIiwidXNlcl9pZCI6Mn0.-EiwwbYFa5XXvp47fYvv3IFaWY45MiqVca_pTloAgoI"}})
         .then(res => res.data)
     },
+
+    async uploadFileUser(data: UploadUserFileDto) {
+        const formattedData = new FormData() 
+        formattedData.append('file', data.files[0])
+    
+        return instance.post('attacment/uploadfiles/user', formattedData, {
+            headers: {
+                "Content-Type": "multipart/form-data"
+            }
+        }).then(res => res)
+    }
 
 }
 
